4445a537d25947df749a3e9596d8c7d441a86489
Muhammed Şaban KARA <81029510+Muhammedska@users.noreply.github.com>
2025-09-01 12:32:07 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import { Card } from 'components/card';
 import { ContextAlert } from 'components/context-alert';
 import { Markdown } from 'components/markdown';
-import { RandomQuote } from 'components/random-quote';
+import { GET } from 'components/random-quote';
 import { getNetlifyContext } from 'utils';
 
 const contextExplainer = `
